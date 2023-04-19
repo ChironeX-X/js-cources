235d3b0bad44d400208e18a3097225f07bd0e3ef
Ladin Pavel <ladinpavel784@gmail.com>
2023-04-19 11:00:28 +0100

    015 my solve

--- public_html/JS/hw.js
+++ public_html/JS/hw.js
@@ -10,12 +10,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько его оцените?', ''),
-        c = prompt('Один из последних просмотренных фильмов?', ''),
-        d = prompt('На сколько его оцените?', '');
 
-personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько его оцените?', '');
+    if (a && b != null && a.length && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else
+    {
+        i--;
+        console.log("error");
+
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов')
+    } else if (10 < personalMovieDB.count < 30) {
+    console.log('любитель');
+    } else if (personalMovieDB.count > 30) {
+    console.log('вы киноман');
+    } else {
+    console.log('произошла ошибка');
+    }
+
 
 console.log(personalMovieDB);
