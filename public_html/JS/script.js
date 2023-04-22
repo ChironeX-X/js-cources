5b90424b1f734be6c3e7d782aaf5c6337df810df
Ladin Pavel <ladinpavel784@gmail.com>
2023-04-22 19:56:12 +0100

    свойства и методы строчных переменных, конспект

--- public_html/JS/script.js
+++ public_html/JS/script.js
@@ -1,2 +1,39 @@
 "use strict";
 
+const str = 'test';
+const arr = [1, 2, 4];
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+// производим поиск подстроки
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+const loggr = "good bruh";
+
+console.log(loggr.slice(3, 6));
+
+console.log(loggr.substring(3, 6));
+
+console.log(loggr.substr(3, 3));
+
+console.log(Math.random());
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+
+const test = "12.2px";
+
+console.log(parseInt(test))
+console.log(parseFloat(test))
+//Самодеятельность, рандомайзер.
+let numRand = Math.random() * 100;
+console.log(parseInt(numRand))
