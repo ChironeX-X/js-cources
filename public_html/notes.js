142eb38086a3670d420e61876454641e6a2da475
Pavel Ladin <ladinpavel784@gmail.com>
2023-06-02 10:27:41 +0100

    033 практика решено

--- public_html/notes.js
+++ public_html/notes.js
@@ -1503,7 +1503,126 @@ createMovieList(movieDB.movies,movieList);
         02.06.23
         Продолжаем с 16:23
         
+        пока исправил мелкие ошибки, будьте внимательны.
+        в отличие от обычных функций function expressions могут быть
+        вызваны только после обьявления
+        
+        'use strict';
 
+//событие для того, чтобы скрипт выполнялся после загрузки дом дерева
+// можно еще сделать так,  чтобы скрипт выполнялся только после загрузки всей страницы
+
+document.addEventListener('DOMContentLoaded',()=>{
+    
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list'),
+      addForm = document.querySelector('form.add'),
+      addInput = addForm.querySelector('.adding__input'),
+      checkBox = addForm.querySelector('[type="checkbox"]');
+      
+      //получение HTML типа через селектор
+      
+    addForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        
+        let newFilm = addInput.value;
+        const favorite = checkBox.checked;
+        
+        if (newFilm) {
+            // по умолчанию пустой запрос это false
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0,22)}...`;
+                //напомню, что это выражение с бэктиками называется интерполяцией
+            };
+            
+            if (favorite){
+                console.log("Добавлен любимый фильм");
+            };
+            
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            createMovieList(movieDB.movies, movieList);
+
+            event.target.reset();
+
+            // этот метод стирает данные формы
+
+        };
+        
+    });
+      
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    
+    //это пример функционального выражения.
+    //function expressions
+    
+    
+    
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    };
+
+    
+    
+    const sortArr = (arr)=>{
+        arr.sort();
+    };
+
+    
+
+    function createMovieList(films, parent) {
+
+        parent.innerHTML = "";
+        sortArr(films);
+        
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+                `;
+
+        });
+        
+        document.querySelectorAll('.delete').forEach((btn,i)=>{
+            btn.addEventListener("click",()=>{
+                btn.parentElement.remove();
+                movieDB.movies.splice(i,1);
+                createMovieList(films, parent);
+            });
+        });
+        
+    };
+
+
+deleteAdv(adv);
+createMovieList(movieDB.movies,movieList);
+makeChanges();
+});
+
+        Просмотрел решение, большая часть из того, что он делал на уроке
+        я тоже сделал, была пара ошибок с тем, что не знал метод дом контент лоадед
+        и то, что функицю можно вызывать рекурсией.
             
  */
 
