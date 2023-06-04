325105d5a01b401c7620f71667c88e4224d9f9f2
Ladin Pavel <ladinpavel784@gmail.com>
2023-06-04 11:29:23 +0100

    034 defer, async динамическое добавление скриптов

--- public_html/JS/script.js
+++ public_html/JS/script.js
@@ -1,1 +1,20 @@
+'use strict';
 
+const p = document.querySelectorAll('p');
+console.log(p);
+
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    //можно и убрать этот аттрибут
+    document.body.append(script);
+    //динамические скрипты ведут себя как async
+    
+};
+
+//из-за того, что мы ставим ассинхронность false
+//Они будут выполнятся друг за другом
+loadScript("js/test.js");
+loadScript("js/Some.js");
