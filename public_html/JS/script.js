7f2321363eeb9ef431041f21969326d915951de7
Ladin Pavel <ladinpavel784@gmail.com>
2023-06-15 04:11:00 +0100

    039 скрипты и время их выполнения 2

--- public_html/JS/script.js
+++ public_html/JS/script.js
@@ -6,10 +6,14 @@
 // третий аргумент передается в функцию.
 
 // можем так же передавать функцию как аргумент
-const timerId = setTimeout(logger, 2000);
+//const timerId = setTimeout(logger, 500);
 //скобки не пишем, функцию не вызываем.
 function logger(){
+    if (i == 3){
+        clearInterval(timerId);
+    };
     console.log('Hello');
+    i++;
 };
 
 //setTimeout(logger, 2000);
@@ -23,13 +27,37 @@ function logger(){
 
 
 const btn = document.querySelector('.btn');
-let timerId;
+let timerId,
+    i = 0;
 //можем создавать пустые глобальные переменные, чтобы они были джоступны 
+//
+//clearInterval(timerId);
+//
+//btn.addEventListener('click',()=>{
+//    //const timerId = setTimeout(logger, 2000);
+//    //Пробуем команду setInterval;
+//    timerId = setInterval(logger, 500);
+//});
+//
+//let id =  setTimeout(function log(){
+//    console.log('Hello');
+//    id = setTimeout(log,500);
+//},500);
 
-btn.addEventListener('click',()=>{
-    //const timerId = setTimeout(logger, 2000);
-    //Пробуем команду setInterval;
-    timerId = setInterval(logger, 2000);
-});
-
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+    
+    const id = setInterval(frame, 10);
+    function frame(){
+        if(pos == 300){
+            clearInterval(id); //сюда вводим idшник
+        } else {
+           pos++;
+           elem.style.top = pos + 'px';
+           elem.style.left = pos +'px';
+        }
+    }
+}
  
+btn.addEventListener('click',myAnimation);
